70f5774a11cf514fb6756d35f098b41936fedcd7
Екатерина Кузькина <ekaterinakuzkina@192-168-0-101.local>
2019-05-25 00:48:09 +0300

    Updates

---
+++ precache-manifest.8b33301453d5fb16ee46838ee5d8e227.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "55fab74e0ca85ded3b2d",
+    "url": "/static/css/main.ae91851f.chunk.css"
+  },
+  {
+    "revision": "55fab74e0ca85ded3b2d",
+    "url": "/static/js/main.bdc8b07a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "058e9e2acc2988cf1f2b",
+    "url": "/static/css/2.9d32bad7.chunk.css"
+  },
+  {
+    "revision": "058e9e2acc2988cf1f2b",
+    "url": "/static/js/2.50710f0c.chunk.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "ec776f384924f5c71832267bda1bd96d",
+    "url": "/index.html"
+  }
+];
